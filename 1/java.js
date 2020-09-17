75bf4314bd755ab02873edf5a85fe06a192a9265
unknown <alextreecko@gmail.com>
2020-09-16 23:50:46 -0500

    JavaScript

---
+++ 1/java.js
@@ -0,0 +1,7 @@
+function showAlert () {
+        window.alert('Gracias por dar clic')
+}
+<p id="dar click" onclick="showAlert();">Hacer Clic</p>
+
+
+
---
+++ 2/archivo.js
@@ -0,0 +1,4 @@
+<div id=”principal”>
+ <p onmouseover="alert('Gracias por dar clic');">Hacer Clic</p>
+ <p>No puede hacer clic</p>
+ </div>
